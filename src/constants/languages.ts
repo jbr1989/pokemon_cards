0cc7d0df38ab5dc496b23167209a5c6ed4a99702
Dizo <jbr1989@gmail.com>
2025-08-24 17:10:42 +0200

    Language flags

---
+++ src/constants/languages.ts
@@ -0,0 +1,27 @@
+export const languages = [
+  {
+    lang: "es",
+    name: "Español",
+    flag: "/img/icon/language/es.webp",
+  },
+  {
+    lang: "en",
+    name: "English",
+    flag: "/img/icon/language/en.webp",
+  },
+  {
+    lang: "ja",
+    name: "日本語 (Japanese)",
+    flag: "/img/icon/language/ja.webp",
+  },
+  {
+    lang: "ko",
+    name: "한국어 (Korean)",
+    flag: "/img/icon/language/ko.webp",
+  },
+  {
+    lang: "zh-tw",
+    name: "繁體中文 (Traditional Chinese)",
+    flag: "/img/icon/language/zh.webp",
+  },
+];

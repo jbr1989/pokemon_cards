74482590ffc6b5d04451a60dea2b0e72f3e248a5
Dizo <jbr1989@gmail.com>
2025-08-29 10:39:31 +0200

    Update PWA

---
+++ public/sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "pokecards-static-v1";
+const PRECACHE_URLS = [
+  "/",
+  "/manifest.webmanifest",
+  "/img/pwa-192x192.png",
+  "/img/pwa-512x512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+  if (request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      const networkFetch = fetch(request)
+        .then((response) => {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(request, responseClone)).catch(() => {});
+          return response;
+        })
+        .catch(() => cached);
+      return cached || networkFetch;
+    })
+  );
+});
+
+
